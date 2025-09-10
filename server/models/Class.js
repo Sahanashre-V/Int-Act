195bfe4c00908447edf3265a0bc1bdf03b8cdfe4
Sachin-2006 <sachin06kaizen@gmail.com>
2025-09-10 17:48:21 +0530

    CURD Operations for class

--- server/models/Class.js
+++ server/models/Class.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 
 const classSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  gradeLevel: { type: String, required: true },    
+  grade: { 
+    type: Number, 
+    required: true, 
+    min: 1, 
+    max: 12 
+  },
+  section: { 
+    type: String, 
+    required: true,
+    trim: true
+  },  
   students: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], 
   teachers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]  
 }, { timestamps: true });
